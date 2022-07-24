f3cac83c62e9024a24c71fa2174ba6338f222ce1
Pankhuri <pmn.bhatagar@gmail.com>
2022-07-24 21:11:31 +0530

    Added User controller actions and header-footer views

---
+++ controllers/users_controller.js
@@ -0,0 +1,20 @@
+// render the User Profile after Logging In
+module.exports.profile = function(req, res){
+    return res.render('user_profile',  {
+        title: 'User Profile'
+    });
+}
+
+// render the Sign Up page
+module.exports.signUp = function(req, res){
+    return res.render('user_signup', {
+        title: 'Petalgram Sign Up'
+    });
+}
+
+// render the Login Page
+module.exports.login = function(req, res){
+    return res.render('user_login', {
+        title: 'Petalgram User Login'
+    });
+}
