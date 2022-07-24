69cdb59d1247ebb8a10f7fd7c4bb36a02917bbed
Pankhuri <pmn.bhatagar@gmail.com>
2022-07-24 20:51:03 +0530

    Add Layout for app

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const db = require('./config/mongoose');
 const users = require('./models/user');
 
 const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use(express.static('./assets'));
 
 app.listen(port, (err)=>{
     if(err){
