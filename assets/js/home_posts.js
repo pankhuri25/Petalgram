99fd4a07a829f0bc27e27f9cdac6cb71e6ec2717
Pankhuri <pmn.bhatnagar@gmail.com>
2022-09-04 22:18:31 +0530

    prepend new post in post list

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,8 +12,11 @@
                 type: 'post',
                 url: '/posts/create-post',
                 data: newPostForm.serialize(), //converts FORM data into Json format
-                success: function(data){
-                    console.log(data);
+                success: function(content){
+                    console.log(content);
+                    let newPost = newPostDom(content.data.post);
+                    // append the post on top (prepend)
+                    $('#posts-list-container > ul').prepend(newPost);
                 },
                 error: function(error){
                     console.log(error.responseText);
@@ -23,6 +26,38 @@
     }
 
     // method to create a post in DOM
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}"> 
+                    <p> 
+                        <small>
+                            <!-- Delete button -->
+                            <a class="delete-post-button" href="/posts/destroy/${post.id}"> x </a>
+                        </small>
+                
+                        <!-- Show post's content -->
+                        ${post.content}
+                        <br>
+                        <!-- Show the user's name who posted it -->
+                        <small>${post.user.name}</small>
+                        
+                    </p>
+                    <div class="post-comments">
+                       
+                        <form action="/comments/add-comment" id="new-comment-form" method="POST">
+                            <textarea name="content" cols="30" rows="3" placeholder="Add a Comment.." required></textarea>
+                            <input type="hidden" name="post" value="${post._id}">
+                            <input type="submit" value="Add Comment">
+                        </form>
+                
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </li>`)
+    }
 
     createPost();
 }
