a5374e0c5c9bc1e6c5f0084c8a38a644df32d41a
Pankhuri <pmn.bhatnagar@gmail.com>
2022-09-19 22:42:24 +0530

    install multer to upload files

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+// import multer and set it up as per requirement for every individual model
+const multer = require('multer');
+
+// import path module to set where the uploads will be kept (destination folder)
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,10 +20,28 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true  //to store when the user was created and updated
 });
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now());  // fieldname --> avatar
+    }
+});
+// console.log("hello: ",path.join(__dirname, '..', AVATAR_PATH));
+
+// create accessible Static methods (like a global function which can be used for each user, OOPs concept)
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');  // single defines that only one single file can be processed, not multiple.
+userSchema.statics.avatarPath = AVATAR_PATH;
+
 
 const User = mongoose.model('User', userSchema);
 
